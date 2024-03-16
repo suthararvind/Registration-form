53d515e40b2d10d42ed961bb3bf4a7d1f742da60
Arvind Kumar <111381098+suthararvind@users.noreply.github.com>
2024-03-16 14:44:11 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose =require("mongoose");
-mongoose.connect("mongodb+srv://suthararvindjlr757:h44t6g6r7@cluster0.b6uhyf9.mongodb.net/?retryWrites=true",{
+mongoose.connect("mongodb+srv://suthararvindjlr757:h44t6g6r7@cluster0.b6uhyf9.mongodb.net/",{
 
 }).then(()=>{
     console.group(`connection successful`);
